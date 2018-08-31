62e9080fefcf76dec529fcab8bd28453b272f819
Chyroc <chyroc@qq.com>
2018-08-31 10:38:27 +0800

    add gofmt goimports mage

--- build.go
+++ build.go
@@ -5,6 +5,7 @@ package main
 import (
 	"os"
 	"runtime"
+	"strings"
 
 	"github.com/magefile/mage/sh"
 )
@@ -26,9 +27,42 @@ func addOSExecType(str string) string {
 }
 
 func Build() error {
-	err:= sh.RunV(goexe, "build", "-o", "bin/vlog", addOSExecType("github.com/Chyroc/vlog/cmd/vlog"))
+	err := sh.RunV(goexe, "build", "-o", "bin/vlog", addOSExecType("github.com/Chyroc/vlog/cmd/vlog"))
 	if err != nil {
 		return err
 	}
 	return sh.RunV(goexe, "build", "-o", "bin/vlog-cli", addOSExecType("github.com/Chyroc/vlog/cmd/vlog-cli"))
+}
+
+func Fmt() error {
+	goFiles, err := findGoFiles()
+	if err != nil {
+		return err
+	}
+
+	return sh.RunV("gofmt", append([]string{"-w"}, goFiles...)...)
+}
+
+func Import() error {
+	goFiles, err := findGoFiles()
+	if err != nil {
+		return err
+	}
+
+	return sh.RunV("goimports", append([]string{"-w", "-local", "github.com/Chyroc/vlog"}, goFiles...)...)
+}
+
+func findGoFiles() ([]string, error) {
+	output, err := sh.Output("find", ".", "-name", "*.go")
+	if err != nil {
+		return nil, err
+	}
+	var files []string
+	for _, v := range strings.Split(output, "\n") {
+		if !strings.HasPrefix(v, "./vendor") {
+			files = append(files, v)
+		}
+	}
+
+	return files, nil
 }
